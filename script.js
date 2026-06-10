9f294311e088c572e441bb5024e08f344a2cd604
varshiniadisamol-27 <varshiniadisamol27@gmail.com>
2026-06-10 06:48:44 +0530

    Add files via upload
    Added Task 1 Responsive Landing Page

---
+++ script.js
@@ -0,0 +1,12 @@
+window.addEventListener("scroll", function() {
+
+    let navbar = document.getElementById("navbar");
+
+    if(window.scrollY > 50){
+        navbar.classList.add("scrolled");
+    }
+    else{
+        navbar.classList.remove("scrolled");
+    }
+
+});
